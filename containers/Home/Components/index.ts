47ebf810c48c344632f920bf8679c9522710046b
Hoi0904108884 <tonthathoi.090302@gmail.com>
2023-10-20 22:14:51 +0700

    update code

--- containers/Home/Components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as DailyWatchingMovie } from "./DailyWatchingMovie";
-export { default as HeadingMovie } from "./HeadingMovie";
-export { default as NowPlayingMovie } from "./NowPlayingMovie";
-export { default as PopularArtist } from "./PopularArtist";
-export { default as TrendingMovie } from "./TrendingMovie";
-export { default as UpcomingMovie } from "./UpcomingMovie";
